53e0442845bcb0b4cb070746aa41ca16a7e911b9
Javapisk <giagea26@gmail.com>
2023-01-26 10:14:10 -0300

    boolean compare

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,13 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 3;
-        a+= 1;
-        a += 5;
+//        int a = 3;
+//        a+= 1;
+//        a += 5;
 //        a -= 1;
 //        a *= 5;
 //        int b = 10;
 //        a /= b;
-        System.out.println(a);
+//        System.out.println(b);
+        int a = 1;
+        int b = 2;
+        boolean compare1 = a< b;
+        System.out.println("Compare1: " + compare1);
+        boolean compare2 = a> b;
+        System.out.println("Compare2: " + compare2);
+
+
 
     }
 }
